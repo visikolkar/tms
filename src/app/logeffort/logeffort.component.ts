aa14e33c43102d02e88b1b849c0669fe94cc14b4
Vishwa Kolkar <visikolkar@gmail.com>
2018-05-14 18:18:33 +0530

    logeffort UI design

--- src/app/logeffort/logeffort.component.ts
+++ src/app/logeffort/logeffort.component.ts
@@ -14,17 +14,33 @@ export class LogeffortComponent implements OnInit {
 		windowheight: any;
 		logefforts: any;
 		logeffortstwo: any;
+		projectName: string;
+		skillSet: string;
+		taskName: string;
+		time = {
+			value: new Date(2018,0,1,0,0,0),
+		};
+		disableSelect: boolean;
+		userLogEffort = {
+			project_name: '',
+			skill_set: '',
+			task_name: '',
+			time: ''
+		};
+		userEffort = [];
     /*  projects,
         tasks,
         commonTasks ==> will be fetched thrupgh a seperate api call
     */
+   
 	projects = [
 		{ value: 'Project A', viewValue: 'Project A' },
 		{ value: 'Project B', viewValue: 'Project B' },
 		{ value: 'Project C', viewValue: 'Project C' },
 		{ value: 'Project D', viewValue: 'Project D' },
+		{ value: 'Common', viewValue: 'Common'}
     ];
-    tasks = [
+    projectTasks = [
 		{ value: 'Task A', viewValue: 'Task A' },
 		{ value: 'Task B', viewValue: 'Task B' },
 		{ value: 'Task C', viewValue: 'Task C' },
@@ -33,9 +49,35 @@ export class LogeffortComponent implements OnInit {
     commonTasks = [
 		{ value: 'Innovation and Tools', viewValue: 'Innovation and Tools' },
 		{ value: 'Leave', viewValue: 'Leave' },
-    ];
+	];
+	
 	constructor() { 
-		
+		// var timeControl = document.getElementById("startTime");
+		// timeControl.value = '00:00';
+		// this.time = timeControl.value;
+	}
+
+	getTime(time): void{
+		console.log('user entered time is', time);
+	}
+
+	disableOption(selected): void {
+		console.log('selected value is', selected);
+		if(selected.value === 'Common'){
+			this.disableSelect = true;
+			this.skillSet = '';
+		} else {
+			this.disableSelect = false;
+		}
+	}
+
+	addUserData(): void {
+		this.userLogEffort.project_name = this.projectName;
+		this.userLogEffort.skill_set = this.skillSet;
+		this.userLogEffort.task_name = this.taskName;
+		this.userLogEffort.time = this.time.value.toDateString();
+		this.userEffort.push(this.userLogEffort);
+		console.log('user filled data is', this.userEffort);
 	}
 
 	ngOnInit() {
@@ -48,6 +90,7 @@ export class LogeffortComponent implements OnInit {
 		//this.logeffortstwo = LOGEFFORTSTWO;
 
 		//console.log('log effort two is ', this.logeffortstwo);
+		
     }
 
 }
