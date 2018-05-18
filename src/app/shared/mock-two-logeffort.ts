f5744fb100d1bef1886393d0d3c6cd7efeae055e
Vishwa Kolkar <visikolkar@gmail.com>
2018-05-18 18:06:34 +0530

    confirm logeffort page ui design

--- src/app/shared/mock-two-logeffort.ts
+++ src/app/shared/mock-two-logeffort.ts
@@ -49,5 +49,50 @@ export const LOGEFFORTSTWO: LogeffortTwo = {
                 time: "4:00"
             }]
         }]
+    }, {
+        irisTime: "09:15 Hours",
+        date: new Date(2018, 3, 9),
+        day: "Monday",
+        isSubmitted: true,
+        isApproved: false,
+        isSelfRejected: false,
+        comments: "Life's Good",
+        manDayStatus: "90",
+        totalLogTime: "8:59 Hours",
+        timeSheet: [{
+            projectName: "Common",
+            jobTask: [{
+                isPrimarySkill: false,
+                isSecondarySkill: false,
+                jobTaskName: "Innovation and Tools",
+                time: "2:30"
+            }, {
+                isPrimarySkill: false,
+                isSecondarySkill: false,
+                jobTaskName: "Leave",
+                time: "2:30"
+            }]
+        }, {
+            projectName: "Project A",
+            jobTask: [{
+                jobTaskName: "Task B",
+                isPrimarySkill: true,
+                isSecondarySkill: false,
+                time: "3:00"
+            }, {
+                jobTaskName: "Task D",
+                isPrimarySkill: false,
+                isSecondarySkill: true,
+                time: "1:00"
+            }]
+        }, {
+            projectName: "Project B",
+            jobTask: [{
+                jobTaskName: "Task A",
+                isPrimarySkill: false,
+                isSecondarySkill: true,
+                time: "4:00"
+            }]
+        }]
     }]
 }
