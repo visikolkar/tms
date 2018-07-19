05a3f8339716f3c08b27385261820170742d7560
Vishwa Kolkar <visikolkar@gmail.com>
2018-07-19 19:35:42 +0530

    [Implementation] Project Status page development

--- src/app/project-status/project-status.component.ts
+++ src/app/project-status/project-status.component.ts
@@ -4,6 +4,7 @@ import { ActivatedRoute } from '@angular/router';
 import { AdminService } from '../admin/admin.service';
 import { LoaderService } from '../loader/loader.service';
 import { MatSnackBar } from '@angular/material';
+import * as moment from 'moment';
 
 @Component({
 	selector: 'app-project-status',
@@ -15,8 +16,26 @@ export class ProjectStatusComponent implements OnInit {
 	projects:any;
 	detailsShow: boolean;
 	addNewShow: boolean;
+	showEditOptions: boolean = false;
 	detailsHeader = 'Project Details';
 	searchText: string;
+	selectedProject: any;
+	dates = {
+		start_date: '',
+		base_ta: ''
+	}
+	addProject = {
+		project_name: '',
+		project_category: '',
+		os_version: '',
+		suffix: '',
+		active: '',
+		project_leader: '',
+		base_ta: '',
+		start_date: '',
+		comments: ''
+	}
+
 	constructor(public sharedService: SharedService,
 		public route: ActivatedRoute,
 		public adminService: AdminService,
@@ -35,6 +54,7 @@ export class ProjectStatusComponent implements OnInit {
 
 	selectProject(obj): void {
 		console.log('selected project is ', obj);
+		this.selectedProject = obj;
 		this.detailsShow = true;
 		this.addNewShow = false;
 		this.detailsHeader = obj.project_name + ' Project Details';
@@ -52,6 +72,32 @@ export class ProjectStatusComponent implements OnInit {
 		this.projects.forEach(function(item){
 			item.selected = false;
 		})
+	}
+
+	editEnable(): void {
+		console.log('click event')
+		this.showEditOptions = true;
+	}
+
+	saveProject(str): void {
+		if(str == 'cancel'){
+			this.showEditOptions = false;
+		} else {
+			//save the project
+		}
+	}
+
+	key_press(e) {
+		//block user from entering in leave dates
+		console.log('event fired');
+		e.preventDefault();
+	}
+
+	addNewProject(){
+		console.log('add project before moment', this.addProject);
+		this.addProject.base_ta = moment(this.dates.base_ta).format("DD-MM-YYYY");
+		this.addProject.start_date = moment(this.dates.start_date).format("DD-MM-YYYY");
+		console.log('add project', this.addProject);
 	}
 
 }
