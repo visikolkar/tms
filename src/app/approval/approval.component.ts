caa055191d877d2af3559b928a078cd11d947251
Vishwa Kolkar <visikolkar@gmail.com>
2018-07-27 22:28:36 +0530

    [Implementation] Reports in approval page

--- src/app/approval/approval.component.ts
+++ src/app/approval/approval.component.ts
@@ -188,37 +188,95 @@ export class ApprovalComponent implements OnInit {
 			var month = +item.iris_date.split('-')[1] - 1;
 			item.displayDate = new Date(item.iris_date.split('-')[2], month, item.iris_date.split('-')[0]).toDateString().slice(0, 10);
 			var dummyReport = [];
+			var dummyTaskReport = [];
+			var dummySkillReport = [];
 			self.totalLogTime(item.empEfforts);
 			item.empEfforts.forEach(function (emp) {
-				emp.summaryEffort = self.summerizeUserEffort(emp.effort);
-				emp.summaryEffort.forEach(e => dummyReport.push(e));
-			})
-			item.report = self.chartReport(dummyReport);
+				if(emp.effort.length){
+					emp.summaryEffort = self.summerizeUserEffort(emp.effort, 'project');
+					emp.taskSummaryEffort = self.summerizeUserEffort(emp.effort, 'task');
+					emp.skillSummaryEffort = self.summerizeUserEffort(emp.effort, 'skill');
+					emp.summaryEffort.forEach(e => dummyReport.push(e));
+					emp.taskSummaryEffort.forEach(e => dummyTaskReport.push(e));
+					emp.skillSummaryEffort.forEach(e => dummySkillReport.push(e));
+				}
+				
+			});
+			item.report = self.chartReport(dummyReport, 'project');
+			item.taskReport = self.chartReport(dummyTaskReport, 'task');
+			item.skillReport = self.chartReport(dummySkillReport, 'skill');
+			//item.taskReport = self.chartReport(dummyReport);
 			item.projectName = [];
 			item.projectEffort = [];
 			item.report.forEach(function (project) {
 				item.projectName.push(project.project_name);
 				item.projectEffort.push(parseFloat(project.time.replace(":", ".")).toFixed(2));
-			})
+			});
+			item.taskName = [];
+			item.taskEffort = [];
+			item.taskReport.forEach(function (task) {
+				item.taskName.push(task.task_name);
+				item.taskEffort.push(parseFloat(task.time.replace(":", ".")).toFixed(2));
+			});
+			item.skillName = [];
+			item.skillEffort = [];
+			item.skillReport.forEach(function (skill) {
+				item.skillName.push(skill.skill_set);
+				item.skillEffort.push(parseFloat(skill.time.replace(":", ".")).toFixed(2));
+			});
 		});
 
 		console.log('approval with summary is ', obj);
 		return obj;
 	}
 
-	chartReport(array) {
+	summerizeUserEffort(arr, str): any {
+		let self = this;
+		var array = JSON.parse(JSON.stringify(arr)); //arr.map(x => Object.assign({}, x)); //deep copy of array (without reference) with this this.userLogEffort will not be effected
+		if(str == 'project'){
+			array.forEach(function (val) {
+				val.task = [{ skill_set: val.skill_set, task_name: val.task_name, hours: val.hours, mins: val.mins }];
+			});
+		} else if(str == 'task') {
+			array.forEach(function (val) {
+				val.project = [{ skill_set: val.skill_set, project_name: val.project_name, hours: val.hours, mins: val.mins }];
+			});
+		} else {
+			array.forEach(function (val) {
+				val.skill = [{ task_name: val.task_name, project_name: val.project_name, hours: val.hours, mins: val.mins }];
+			});
+		}
+
+		var out = this.chartReport(array, str);
+		return out;
+	}
+
+	chartReport(array, str) {
 		var self = this;
 		var output = [];
 
 		array.forEach(function (value) {
 			console.log('output is', output);
 			var existing = output.filter(function (v, i) {
-				return v.project_name == value.project_name;
+				if(str == 'project'){
+					return v.project_name == value.project_name;
+				} else if(str == 'task') {
+					return v.task_name == value.task_name;
+				} else {
+					return v.skill_set == value.skill_set;
+				}
 			});
-			console.log('existing is', existing);
+			console.log(str + 'existing is', existing);
+			
 			if (existing.length) {
 				var existingIndex = output.indexOf(existing[0]);
-				output[existingIndex].task = output[existingIndex].task.concat(value.task);
+				if(str == 'project'){
+					output[existingIndex].task = output[existingIndex].task.concat(value.task);
+				} else if(str == 'task') {
+					output[existingIndex].project = output[existingIndex].project.concat(value.project);
+				} else {
+					output[existingIndex].skill = output[existingIndex].skill.concat(value.skill);
+				}
 			} else {
 				output.push(value);
 			}
@@ -226,18 +284,25 @@ export class ApprovalComponent implements OnInit {
 
 		console.log('final output is', output);
 		output = this.prepareUserEffort(output);
+		if(str == 'project'){
+			var rep = 'task';
+		} else if(str == 'task'){
+			var rep = 'project';
+		} else {
+			var rep = 'skill';
+		}
 		output.forEach(function (item) {
-			if (item.task.length > 1) {
-				var totalHours = item.task.reduce(function (v, n) {
+			if (item[rep].length > 1) {
+				var totalHours = item[rep].reduce(function (v, n) {
 					return v + +n.hours;
 				}, 0);
-				var totalMins = item.task.reduce(function (v, n) {
+				var totalMins = item[rep].reduce(function (v, n) {
 					return v + +n.mins;
 				}, 0);
 
 				item.time = self.minsToHours(totalHours, totalMins);
 			} else {
-				item.time = self.minsToHours(+item.task[0].hours, +item.task[0].mins);
+				item.time = self.minsToHours(+item[rep][0].hours, +item[rep][0].mins);
 			}
 
 		})
@@ -298,17 +363,6 @@ export class ApprovalComponent implements OnInit {
 		});
 		prepareArray = this.filterUserEffort(prepareArray);
 		return prepareArray;
-	}
-
-	summerizeUserEffort(arr): any {
-		let self = this;
-		var array = JSON.parse(JSON.stringify(arr)); //arr.map(x => Object.assign({}, x)); //deep copy of array (without reference) with this this.userLogEffort will not be effected
-		array.forEach(function (val) {
-			val.task = [{ skill_set: val.skill_set, task_name: val.task_name, hours: val.hours, mins: val.mins }];
-		});
-
-		var out = this.chartReport(array);
-		return out;
 	}
 
 	ngOnInit() {
@@ -387,6 +441,48 @@ export class ApprovalComponent implements OnInit {
 			display: true,
 			position: 'top',
 			text: 'Project Wise Analysis'
+		},
+		elements: {
+			radius: 20
+		}
+	};
+	public taskOptions = {
+		legend: {
+			display: true,
+			position: 'right',
+			labels: {
+				fontColor: 'black',
+				fontSize: 12,
+				position: 'bottom',
+				usePointStyle: true,
+				boxWidth: 100
+			}
+		},
+		title: {
+			display: true,
+			position: 'top',
+			text: 'Task Wise Analysis'
+		},
+		elements: {
+			radius: 20
+		}
+	};
+	public skillOptions = {
+		legend: {
+			display: true,
+			position: 'right',
+			labels: {
+				fontColor: 'black',
+				fontSize: 12,
+				position: 'bottom',
+				usePointStyle: true,
+				boxWidth: 100
+			}
+		},
+		title: {
+			display: true,
+			position: 'top',
+			text: 'Skill Wise Analysis'
 		},
 		elements: {
 			radius: 20
