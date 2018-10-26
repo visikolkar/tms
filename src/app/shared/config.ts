c91297623bdafc60be5bd06fba3c54942f538ada
Vishwa Kolkar <visikolkar@gmail.com>
2018-10-26 17:40:17 +0530

    base href and deploy url issue fix for angular 6

--- src/app/shared/config.ts
+++ src/app/shared/config.ts
@@ -1,4 +1,4 @@
-// export const SERVER_URL: string = 'http://si-ramesh86:8080/TMS2/'; //dev server
+// export const SERVER_URL: string = 'http://si-rameshkn:8080/TMS2/'; //dev server
 //export const SERVER_URL: string = 'http://10.221.31.34:8080/TMS2/'; //prod server
 // export const SERVER_URL: string = 'http://172.24.1.204:8080/TMS2/'; //IT server
 export const SERVER_URL: string = 'http://qtms.lge.com:8080/TMS2/'; //IT server
